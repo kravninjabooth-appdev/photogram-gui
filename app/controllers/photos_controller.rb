08965fd5d2e9539ba0300c75f51dc972635263f5
kravninja <tita0@chicagobooth.edu>
2022-11-11 18:16:12 +0000

    updated photos controller and photo show page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,6 +10,13 @@ class PhotosController < ApplicationController
 
   def show
 
+    #Parameters: {"path_id"=>"777"}
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({:id => url_id})
+
+    @the_photo = matching_photos.at(0)
+
     render({:template => "photo_templates/show.html.erb"})
   end
 
